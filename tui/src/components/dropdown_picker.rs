d9a4f5d32a64a1dcf3b98bedc06fef2416a664d1
botox <botox@blockhackers.io>
2026-06-28 06:03:30 +0000

    add dropdown picker instructions

---
+++ tui/src/components/dropdown_picker.rs
@@ -0,0 +1,9 @@
+// TODO: dropdown text picker
+//  Make a text field that is similar to text_field.rs however have it accept a list of T which can
+//  be rendered as a component similar to list.rs. Contrait the type so that it is searchable
+//  - the text field should look like a normal text field but typing in it automatically filters the
+//  list and displays the dropdown list overlayed over everything else.
+//  - The list is capped at 7 results below the field
+//  - make as many components as makes sense, more components often makes composition easier.
+//  - demonstrate the component by adding to the demo
+//  - be sure to demonstrate the overlay effect masking the underlying content
